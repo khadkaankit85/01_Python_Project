44fadbdb9b35e954834d47c605fa083f5544deed
Ankit Khadka <khadkaankit85@gmail.com>
2023-12-18 22:02:10 -0500

    making Classes

---
+++ main.py
@@ -0,0 +1,50 @@
+# Display a menu with options such as adding a task, viewing tasks, marking tasks as completed, and quitting the application.
+def menu():
+    print("1. Add a Task.")
+    print("2. View Tasks.")
+    print("3. Marking Tasks as Completed.")
+    print("4. Quit.")
+    
+# Task Management:
+
+# Users should be able to add tasks with a description, due date, and priority.
+# Tasks should have a status (e.g., incomplete or completed).
+# def manager():
+#     task=input("Task: ")
+#     description=input("Description: ")  
+#     due_date=input("")  
+#     priority=input("")
+class Task:
+    def __init__(self,Task,description,due_date,not_selected,unupdated):
+        self.task=Task
+        self.description=description
+        self.due_date=due_date
+        self.priority=not_selected
+        self.status=unupdated
+        with open("taskproject.txt","a") as project_file:
+            project_file.write(self.task,self.description,self.due_date,self.priority,self.status,"\n")
+        
+    def task_details(self):
+        print(f"{self.task}: {self.description}")
+        print(f"Due: {self.due_date}" )
+        print(f"Status: {self.status}")
+    
+    
+    
+    
+    
+    
+    
+    
+while True: 
+ menu()
+ selection=input("Enter Your Choice here: ")
+ if int(selection)==1:
+     pass
+ elif int(selection)==2:
+     pass
+ elif int(selection)==3:
+     pass
+ elif int(selection)==4:
+     print("See You Around!!")
+     break
